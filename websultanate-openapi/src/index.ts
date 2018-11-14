406fc52ef94cd36fc5870700d63a5ff2b9088e90
anujm4467 <anujm4467@gmail.com>
2018-11-15 04:41:56 +0530

    done

---
+++ websultanate-openapi/src/index.ts
@@ -0,0 +1,16 @@
+import {WebsultanateApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {WebsultanateApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new WebsultanateApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
---
+++ websultanate-openapi/test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {WebsultanateApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new WebsultanateApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: WebsultanateApplication;
+  client: Client;
+}
